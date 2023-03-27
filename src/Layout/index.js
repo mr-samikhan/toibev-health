6538188d8b0a80be3ae7f0459a795e020eb97abe
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-03-28 02:38:49 +0500

    feat: Layout Component

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
+import BottomNav from "../components/BottomNavBar";
 import { Header } from "../components/Header";
+import { useMediaQuery, Box } from "@mui/material";
+import "./layout.scss";
 
 export default function Layout() {
+  const matches = useMediaQuery("(max-width: 900px)");
   return (
     <div style={{ display: "flex" }}>
       <div className="sidebar" id="sidebar">
         <Sidebar />
       </div>
+      {/* {!matches && (
+        <div className="sidebar" id="sidebar">
+          <Sidebar />
+        </div>
+      )} */}
       <div className="main-content">
         <Header />
-        <main className="main" id="detail">
+        <main className={matches ? "main-mobile" : "main"} id="detail">
           <Outlet />
         </main>
       </div>
+      {matches && (
+        <Box sx={{ display: { xs: "block", md: "none" }, marginTop: "50px" }}>
+          <BottomNav />
+        </Box>
+      )}
     </div>
   );
 }
