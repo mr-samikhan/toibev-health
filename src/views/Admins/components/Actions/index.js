8dec9bf302bc3ab88844dcbd467f8f458501349c
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-03-28 02:34:42 +0500

    feat: Admin listing Actions

--- src/views/Admins/components/Actions/index.js
+++ src/views/Admins/components/Actions/index.js
@@ -1,21 +1,35 @@
 import { IconButton, Grid } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import icons from "../../../../assets";
+import AlertDialog from "../../../../components/AlertDialog";
+import ManageAdminForm from "../ManageAdminForm";
 
-export function Actions() {
+export function Actions({ data }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Grid container justifyContent="flex-end">
-      {" "}
-      <Grid item sx={{ paddingTop: "0px" }}>
-        <IconButton>
-          <img src={icons.editIcon} />
-        </IconButton>
+    <>
+      {open && (
+        <AlertDialog
+          open={open}
+          title="Manage Admin"
+          setOpen={setOpen}
+          message={<ManageAdminForm data={data} />}
+        />
+      )}
+      <Grid container justifyContent="flex-end">
+        {" "}
+        <Grid item sx={{ paddingTop: "0px" }}>
+          <IconButton onClick={() => setOpen(true)}>
+            <img src={icons.editIcon} />
+          </IconButton>
+        </Grid>
+        <Grid item sx={{ paddingTop: "0px" }}>
+          <IconButton>
+            <img src={icons.deleteIcon} />
+          </IconButton>
+        </Grid>
       </Grid>
-      <Grid item sx={{ paddingTop: "0px" }}>
-        <IconButton>
-          <img src={icons.deleteIcon} />
-        </IconButton>
-      </Grid>
-    </Grid>
+    </>
   );
 }
