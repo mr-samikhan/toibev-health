f73ce6c9884b493aa136ea657eb79b227b784e69
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-03-28 02:33:47 +0500

    feat:Admins Listings

--- src/views/Admins/index.js
+++ src/views/Admins/index.js
@@ -1,12 +1,32 @@
-import { Avatar, Grid, Typography, Button } from "@mui/material";
-import React from "react";
+import {
+  Avatar,
+  Grid,
+  Typography,
+  Button,
+  useMediaQuery,
+  Divider,
+} from "@mui/material";
+import React, { useState } from "react";
 import { Actions } from "./components/Actions";
 import AddIcon from "@mui/icons-material/Add";
 import "./style.scss";
+import AdminsTable from "./components/AdmimsTable";
+import AlertDialog from "../../components/AlertDialog";
+import AddAdminForm from "./components/AddAdminForm";
 
 export function Admins() {
+  const matches = useMediaQuery("(max-width: 600px)");
+  const [open, setOpen] = useState(false);
   return (
     <>
+      {open && (
+        <AlertDialog
+          open={open}
+          setOpen={setOpen}
+          title="Add Admin"
+          message={<AddAdminForm />}
+        />
+      )}
       <Grid
         container
         justifyContent="flex-end"
@@ -18,66 +38,15 @@ export function Admins() {
             color="primary"
             variant="contained"
             startIcon={<AddIcon />}
-            className="contained-button"
+            className={matches ? "contained-icon-button" : "contained-button"}
+            onClick={() => setOpen(true)}
           >
             {" "}
-            Add
+            {!matches && "Add"}
           </Button>
         </Grid>
       </Grid>
-      <Grid sx={{ overfloxX: "scroll" }}>
-        <Grid
-          container
-          className="table-header"
-          justifyContent="space-between"
-          sx={{ flexWrap: "nowrap" }}
-        >
-          <Grid item className="item" xs={3} sx={{ minWidth: "200px" }}>
-            <Typography sx={{ marginLeft: "76px" }}>Username</Typography>
-          </Grid>
-          <Grid item className="item" xs={3} sx={{ minWidth: "200px" }}>
-            <Typography>Email Address</Typography>
-          </Grid>
-          <Grid item className="item" xs={3} sx={{ minWidth: "200px" }}>
-            <Typography>Permission Level</Typography>
-          </Grid>
-          <Grid item className="item" xs={3} sx={{ minWidth: "200px" }}></Grid>
-        </Grid>
-
-        {[1, 2, 3, 4, 1, 2, 3, 12, 3].map(() => (
-          <Grid container className="row">
-            <Grid
-              item
-              container
-              justifyContent="space-between"
-              alignItems="center"
-              className="wrapper"
-              sx={{ flexWrap: "nowrap" }}
-            >
-              <Grid item xs={3} sx={{ minWidth: "200px" }}>
-                <Grid container alignItems="center">
-                  <Grid item>
-                    <Avatar className="avatar" />
-                  </Grid>
-                  <Grid item>
-                    <Typography className="text">Chop Dawg</Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs={3} sx={{ minWidth: "200px" }}>
-                <Typography className="text">partner@chopdawg.com</Typography>
-              </Grid>
-              <Grid item xs={3} sx={{ minWidth: "200px" }}>
-                <Typography className="text">Administrator</Typography>
-              </Grid>
-
-              <Grid item xs={3} sx={{ minWidth: "200px" }}>
-                <Actions />
-              </Grid>
-            </Grid>
-          </Grid>
-        ))}
-      </Grid>
+      <AdminsTable />
     </>
   );
 }
