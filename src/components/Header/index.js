ca2f42f7d80c9e3f80f5cdd7e9516b6886be468f
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-03-28 02:38:27 +0500

    feat: Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,46 +1,30 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import {
   Avatar,
   Grid,
-  InputAdornment,
   TextField,
   Typography,
+  InputAdornment,
 } from "@mui/material";
-// import { drawerWidth } from "../mocks/menuList";
 import { ReactComponent as BellImage } from "../../assets/images/bell.svg";
 import { ReactComponent as MenImage } from "../../assets/images/men.svg";
 import { ReactComponent as DropdownIcon } from "../../assets/images/dropdown.svg";
 import { ReactComponent as NotificationBadge } from "../../assets/icons/notificationbadge.svg";
 import { ReactComponent as DownArrow } from "../../assets/icons/downarrow.svg";
 import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
+import { ReactComponent as HamburgerIcon } from "../../assets/icons/hamburger.svg";
+import { ReactComponent as Logo } from "../../assets/icons/header-logo.svg";
+import { useHeader } from "./useHeader";
+
 import "../Sidebar/sidebar.scss";
 import "./header.scss";
 import { DatePicker } from "../DatePicker";
-const drawerWidth = 240;
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
 
-export const Header = ({ childData, handleDrawerToggle, title = "Learn" }) => {
+export const Header = ({ childData, handleDrawerToggle }) => {
+  const { title, drawerWidth, AppBar, dispatch, matches } = useHeader();
   return (
     <Box className="header">
       <AppBar
@@ -49,7 +33,7 @@ export const Header = ({ childData, handleDrawerToggle, title = "Learn" }) => {
         sx={{
           background: {
             xs: (theme) => theme.palette.primary.main,
-            sm: "#fff",
+            md: "#fff",
           },
         }}
         elevation={0}
@@ -62,44 +46,86 @@ export const Header = ({ childData, handleDrawerToggle, title = "Learn" }) => {
             alignItems: "center",
           }}
         >
-          <Grid container justifyContent={"space-between"}>
-            <Grid item>
-              <Typography className="title">{title}</Typography>
-            </Grid>
-            <Grid item>
-              <Grid container alignItems="center">
-                <Grid item>
-                  <DatePicker />
-                </Grid>
-                <Grid item sx={{ margin: "0px 24px 0px 32px" }}>
-                  <IconButton>
-                    {" "}
-                    <NotificationBadge />
+          {!matches ? (
+            <Grid container flexDirection="column">
+              <Grid
+                item
+                container
+                justifyContent="space-between"
+                alignItems="center"
+                mb={2}
+              >
+                <Grid item ml={-1}>
+                  <IconButton
+                    onClick={() => dispatch({ type: "TOGGLESIDEBAR" })}
+                  >
+                    <HamburgerIcon />
                   </IconButton>
+                </Grid>
+                <Grid item>
+                  <Logo />
                 </Grid>
                 <Grid item>
                   <Avatar />
                 </Grid>
-                <Grid item>
-                  <Typography className="username">Alfonso Gouse</Typography>
-                </Grid>
-                <Grid item>
-                  <IconButton>
-                    <DownArrow />
-                  </IconButton>
+              </Grid>
+              <Grid item mb={3}>
+                <TextField
+                  fullWidth
+                  className="search-field"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />{" "}
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item mb={1}>
+                <Typography className="header-title">{title}</Typography>
+              </Grid>
+            </Grid>
+          ) : (
+            <Grid container justifyContent={"space-between"}>
+              <Grid item>
+                <Typography className="title">{title}</Typography>
+              </Grid>
+              <Grid item>
+                <Grid container alignItems="center">
+                  <Grid item>
+                    <DatePicker />
+                  </Grid>
+                  <Grid item sx={{ margin: "0px 24px 0px 32px" }}>
+                    <IconButton>
+                      {" "}
+                      <NotificationBadge />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <Avatar />
+                  </Grid>
+                  <Grid item>
+                    <Typography className="username">Alfonso Gouse</Typography>
+                  </Grid>
+                  <Grid item>
+                    <IconButton>
+                      <DownArrow />
+                    </IconButton>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          )}
         </Toolbar>
-        <Box
+        {/* <Box
           sx={{
             display: { xs: "block", sm: "none" },
             // minHeight: '200px',
             background: (theme) => theme.palette.primary.main,
             borderBottomRightRadius: "20px",
           }}
-        ></Box>
+        ></Box> */}
       </AppBar>
     </Box>
   );
---
+++ src/components/Header/useHeader.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import { useMediaQuery } from "@mui/material";
+import { useDispatch } from "react-redux";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export function useHeader() {
+  const { pathname } = useLocation();
+  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+  const matches = useMediaQuery("(min-width: 900px)");
+  useEffect(() => {
+    setTitle(
+      pathname === "/dashboard"
+        ? "Dashboard"
+        : pathname === "/learn"
+        ? "Learn"
+        : pathname === "/health"
+        ? "Health"
+        : pathname === "/events"
+        ? "Events"
+        : pathname === "/information"
+        ? "Information"
+        : pathname === "/assesment"
+        ? "Assessment"
+        : pathname === "/admins"
+        ? "Admins"
+        : pathname === "/account-settings"
+        ? "Account Settings"
+        : ""
+    );
+  }, [pathname]);
+  return { title: title, drawerWidth, AppBar, dispatch, matches };
+}
