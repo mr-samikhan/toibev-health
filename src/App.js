03b135d352f4f98fdde690f4d2a598648c68a973
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-06-12 03:41:46 +0500

    feat: added query client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Router } from "./Router/router";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 function App() {
-  return <Router />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
