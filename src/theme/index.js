c70d6411c7cb4620d37fa1ad97e34177367df77c
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-06-12 03:38:21 +0500

    fix: theme fixes

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,34 +3,41 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     primary: {
-      light:'#fff',
-      main: '#468D8D',
-      dark:'#000'
+      light: "#fff",
+      main: "#468D8D",
+      dark: "#000",
     },
     secondary: {
-      light:'gba(242, 106, 71, 0.08)',
-      main: '#F26A47',
+      light: "gba(242, 106, 71, 0.08)",
+      main: "#F26A47",
     },
   },
   typography: {
     allVariants: {
       fontFamily: "DM Sans",
     },
+    h6: {
+      fontStyle: "normal",
+      fontWeight: "500",
+      fontSize: "18px",
+      lineHeight: "23px",
+      color: "#000000",
+    },
   },
   components: {
     MuiDrawer: {
       styleOverrides: {
         paper: {
-          backgroundColor: '#468D8D',
-        }
-      }
+          backgroundColor: "#468D8D",
+        },
+      },
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root : {
-          borderRadius: '15px'
-        }
-      }
-    }
-  }
+        root: {
+          borderRadius: "15px",
+        },
+      },
+    },
+  },
 });
