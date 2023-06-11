2489256727d7138e0c50a97c331edae6e37bf8e2
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-06-12 03:37:26 +0500

    feat: custom hook to get all assessment questions from backend

---
+++ src/hooks/useGetAssessmentQuestions.js
@@ -0,0 +1,40 @@
+import { useQuery } from "react-query";
+import { firestore, collection, getDocs, getDoc, doc } from "../firebase";
+import MultipleAnswers from "../views/Assessment/components/MultipleAnswers";
+
+const fetchInfo = async (id) => {
+  let questions = [];
+  try {
+    const querySnapshot = await getDocs(
+      collection(firestore, "Assessments", `${id}`, "questions")
+    );
+
+    querySnapshot.forEach((document) => {
+      let question = {
+        id: document.id,
+        title: document.data().question,
+        subtitle: <MultipleAnswers data={document.data().answers} />,
+        ...document.data(),
+      };
+      questions.push(question);
+    });
+
+    return questions;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const useGetAssessmentQuestions = ({ enabled = true, id }) => {
+  const { data, isLoading, error, isFetching } = useQuery(
+    ["get-assessment-questions"],
+    () => fetchInfo(id),
+    {
+      enabled,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  return { isLoading, error, questions: data, isFetching };
+};
