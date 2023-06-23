6bc886c23ef2faab1f3c7f970783168d14498c27
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-06-23 18:27:31 +0500

    fix: added search in list in card listing

--- src/components/CardListing/index.js
+++ src/components/CardListing/index.js
@@ -1,12 +1,49 @@
-import React from "react";
-import { Avatar, Grid, Typography, IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Grid,
+  Typography,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import { ReactComponent as SearchIcon } from "../../assets/icons/search-normal.svg";
 import icons from "../../assets";
 import "./style.scss";
 
 export default function CardListing({ list, CardActionButton }) {
+  const [data, setData] = useState(list);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    if (searchValue.length) {
+      const items = [...data];
+      const filteredItems = items.filter((item) =>
+        item.title.toLowerCase().includes(searchValue.toLowerCase())
+      );
+
+      setData(filteredItems);
+    } else setData(list);
+  }, [searchValue]);
+
   return (
     <Grid container className="card-listing">
-      {list?.map((item, index) => (
+      <Grid item xs={12} sx={{ mb: { xs: 3, sm: 2 } }} className="search-field">
+        {" "}
+        <TextField
+          fullWidth
+          placeholder="Search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" sx={{ pl: 2 }}>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+      </Grid>
+      {data?.map((item, index) => (
         <Grid item xs={12} className="card-list-item" key={index}>
           <ListItem item={item} CardActionButton={CardActionButton} />
         </Grid>
