4a53ae0e8651d205707d38c5e69c30d62bdc553d
faisalaslam96 <109282080+faisalaslam96@users.noreply.github.com>
2023-08-16 19:51:26 +0800

    feat: custom date picker component

---
+++ src/components/CustomDatePicker/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
+import DatePicker from "@hassanmojab/react-modern-calendar-datepicker";
+import { ReactComponent as CalenderIcon } from "../../assets/icons/calendar.svg";
+import { styled } from "@mui/material/styles";
+
+export const CustomIconButton = styled((props) => <IconButton {...props} />)(
+  ({ theme }) => ({
+    "&.MuiIconButton-root": {
+      background: "rgba(242, 106, 71, 0.08)",
+      borderRadius: "10px",
+      marginRight: "8px",
+      filter:
+        "invert(50%) sepia(97%) saturate(1979%) hue-rotate(334deg) brightness(102%) contrast(90%)",
+    },
+    "& svg": {
+      width: "16px",
+      height: "16px",
+    },
+  })
+);
+
+const CustomTextField = styled((props) => <TextField focused {...props} />)(
+  ({ theme }) => ({
+    "&.MuiTextField-root": {
+      "& input": {
+        width: "unset",
+        flexGrow: 1,
+      },
+      "& .MuiFormLabel-root": { color: "#000" },
+      "& .MuiOutlinedInput-notchedOutline": {
+        border: "1px solid #DCDCDC",
+        borderRadius: "14px",
+      },
+      transition: theme.transitions.create([
+        "border-color",
+        "background-color",
+        "box-shadow",
+      ]),
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+        // borderColor: theme.palette.primary.main,
+        // borderWidth: "2px",
+        border: "1px solid #DCDCDC",
+        borderRadius: "14px",
+      },
+      "& fieldset": {
+        padding: "0px 24px",
+      },
+      "& label": { left: "16px" },
+      "& input": { paddingLeft: "24px" },
+    },
+  })
+);
+
+export default function CustomDatePicker() {
+  const [selectedDay, setSelectedDay] = useState(null);
+
+  const renderCustomInput = ({ ref }) => (
+    <CustomTextField
+      disabled
+      id="outlined"
+      label="Date"
+      placeholder="Select date"
+      inputRef={ref}
+      value={
+        selectedDay
+          ? `${selectedDay.year}/${selectedDay.month}/${selectedDay.day}`
+          : ""
+      }
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <CustomIconButton>
+              <CalenderIcon />
+            </CustomIconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+
+  return (
+    <DatePicker
+      value={selectedDay}
+      onChange={setSelectedDay}
+      inputPlaceholder="Select a day"
+      shouldHighlightWeekends
+      renderInput={renderCustomInput}
+    />
+  );
+}
