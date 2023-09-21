d336e8fb3a1ad2c973ea941d27ffe231a2c8c3d2
sami-chopdawg <sami@chopdawg.com>
2023-09-21 17:26:46 +0500

    feat:added snack-bar for success and error

--- src/views/Admins/components/ActionButtons/index.js
+++ src/views/Admins/components/ActionButtons/index.js
@@ -1,13 +1,24 @@
-import { IconButton, Grid } from "@mui/material";
-import React, { useState } from "react";
-import icons from "../../../../assets";
-import AlertDialog from "../../../../components/AlertDialog";
-import DeleteAdminForm from "../Forms/DeleteAdminForm";
-import AddAdminForm from "../Forms/AddAdminForm";
+import React, { useState } from 'react'
+import { IconButton, Grid } from '@mui/material'
+
+//imports
+import icons from '../../../../assets'
+import AddAdminForm from '../Forms/AddAdminForm'
+import DeleteAdminForm from '../Forms/DeleteAdminForm'
+import AlertDialog from '../../../../components/AlertDialog'
+import MuiSnackbar from '../../../../components/MuiSnackbar/MuiSnackbar'
 
 export function Actions({ data }) {
-  const [open, setOpen] = useState(false);
-  const [openDeleteAdminForm, setOpenDeleteAdminForm] = useState(false);
+  const [open, setOpen] = useState(false)
+
+  const [showAlert, setShowAlert] = useState({
+    open: false,
+    message: '',
+    isError: false,
+  })
+  const [openDeleteAdminForm, setOpenDeleteAdminForm] = useState(false)
+
+  const handleClose = () => setShowAlert((prev) => ({ ...prev, open: false }))
 
   return (
     <>
@@ -16,31 +27,45 @@ export function Actions({ data }) {
           open={open}
           title="Manage Admin"
           setOpen={setOpen}
-          message={<AddAdminForm data={data} isEdit setOpen={setOpen} />}
+          message={
+            <AddAdminForm
+              isEdit
+              data={data}
+              setOpen={setOpen}
+              setShowAlert={setShowAlert}
+            />
+          }
         />
       )}
       {openDeleteAdminForm && (
         <AlertDialog
           maxWidth="xs"
-          open={openDeleteAdminForm}
           title="Delete Admin"
+          open={openDeleteAdminForm}
           setOpen={setOpenDeleteAdminForm}
-          message={<DeleteAdminForm data={data} />}
+          message={<DeleteAdminForm data={data} setShowAlert={setShowAlert} />}
         />
       )}
       <Grid container justifyContent="flex-end">
-        {" "}
-        <Grid item sx={{ paddingTop: "0px" }}>
+        <Grid item sx={{ paddingTop: '0px' }}>
           <IconButton onClick={() => setOpen(true)}>
-            <img src={icons.editIcon} />
+            <img src={icons.editIcon} alt="edit-icon" />
           </IconButton>
         </Grid>
-        <Grid item sx={{ paddingTop: "0px" }}>
+        <Grid item sx={{ paddingTop: '0px' }}>
           <IconButton onClick={() => setOpenDeleteAdminForm(true)}>
-            <img src={icons.deleteIcon} />
+            <img src={icons.deleteIcon} alt="delete-icon" />
           </IconButton>
         </Grid>
       </Grid>
+      {showAlert.open && (
+        <MuiSnackbar
+          open={showAlert.open}
+          setOpen={handleClose}
+          isError={showAlert.isError}
+          message={showAlert?.message}
+        />
+      )}
     </>
-  );
+  )
 }
