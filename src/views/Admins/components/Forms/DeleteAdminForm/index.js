7e90090bec94ed8d84d615ce775545dec60b71b5
sami-chopdawg <sami@chopdawg.com>
2023-09-21 17:27:08 +0500

    feat:added loading to button

--- src/views/Admins/components/Forms/DeleteAdminForm/index.js
+++ src/views/Admins/components/Forms/DeleteAdminForm/index.js
@@ -1,14 +1,16 @@
-import { Typography } from "@mui/material";
-import React from "react";
-import { useMutation, useQueryClient } from "react-query";
-import { useForm } from "react-hook-form";
-import { Grid, Box } from "@mui/material";
-import CustomButton from "../../../../../components/CustomButton";
-import { deleteAdmin } from "../../../actions";
-import useDeleteAdmin from "../../../hooks/useDeleteAdmin";
+import { Typography } from '@mui/material'
+import React from 'react'
+import { Grid, Box } from '@mui/material'
 
-export default function DeleteAdminForm({ data }) {
-  const { handleSubmit, onSubmit, isLoading } = useDeleteAdmin({ data });
+//imports
+import useDeleteAdmin from '../../../hooks/useDeleteAdmin'
+import CustomButton from '../../../../../components/CustomButton'
+
+export default function DeleteAdminForm({ data, setShowAlert }) {
+  const { handleSubmit, onSubmit, isLoading } = useDeleteAdmin({
+    data,
+    setShowAlert,
+  })
 
   return (
     <Box component="form" onSubmit={handleSubmit(onSubmit)}>
@@ -18,10 +20,10 @@ export default function DeleteAdminForm({ data }) {
         </Grid>
         <Grid item xs={12}>
           <CustomButton variant="contained" type="submit">
-            Delete
+            {isLoading ? 'Loading...' : 'Delete'}
           </CustomButton>
         </Grid>
       </Grid>
     </Box>
-  );
+  )
 }
