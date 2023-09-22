7be5f9fedfa29121ae81bbb062036d15194ef8cc
sami-chopdawg <sami@chopdawg.com>
2023-09-22 08:56:46 +0500

    feat:hoc modified

--- src/HOC/ProtectedRoutes.js
+++ src/HOC/ProtectedRoutes.js
@@ -1,41 +1,46 @@
-import React, { useEffect, useLayoutEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
-import { useSelector } from "react-redux";
+import React, { useEffect, useLayoutEffect } from 'react'
+
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const ProtectedRoutes = ({ children }) => {
-  const { search } = useLocation();
+  const { search } = useLocation()
 
   const { userToken, isAuthenticated, isLoading } =
-    useSelector((state) => state?.Auth) ?? {};
-  const navigate = useNavigate();
+    useSelector((state) => state?.Auth) ?? {}
+
+  const navigate = useNavigate()
+
   useLayoutEffect(() => {
     if (userToken && isAuthenticated) {
-      axios.defaults.headers.common["Authorization"] = `Bearer ${userToken}`;
+      axios.defaults.headers.common['Authorization'] = `Bearer ${userToken}`
     } else {
-      delete axios.defaults.headers.common["Authorization"];
-      search.includes("mode=resetPassword")
+      delete axios.defaults.headers.common['Authorization']
+      search.includes('mode=resetPassword')
         ? navigate(`/reset-password${search}`)
-        : navigate("/login");
+        : navigate('/login')
     }
     // eslint-disable-next-line
-  }, [location, isAuthenticated, userToken]);
+  }, [location, isAuthenticated, userToken])
+
+  // console.log(userToken, isAuthenticated, isLoading, '>>>>>>>>>test')
 
   useEffect(() => {
     if (!userToken || !isAuthenticated) {
-      search.includes("mode=resetPassword")
+      search.includes('mode=resetPassword')
         ? navigate(`/reset-password${search}`)
-        : navigate("/login");
+        : navigate('/login')
     }
-  }, []);
+  }, [])
 
   return isLoading ? (
-    "LOADING..."
+    'LOADING...'
   ) : userToken && isAuthenticated ? (
-    <>{children}</>
+    <React.Fragment>{children}</React.Fragment>
   ) : (
     <></>
-  );
-};
+  )
+}
 
-export default ProtectedRoutes;
+export default ProtectedRoutes
