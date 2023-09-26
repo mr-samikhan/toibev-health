571214b53b32d1cd929c4a91c0149cd8cc0c79a7
sami-chopdawg <sami@chopdawg.com>
2023-09-26 18:04:49 +0500

    feat:redux work added for saving date globally

--- src/components/Header/useHeader.js
+++ src/components/Header/useHeader.js
@@ -1,88 +1,108 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { styled } from "@mui/material/styles";
-import MuiAppBar from "@mui/material/AppBar";
-import { useMediaQuery } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { resetAuthValues } from "../../redux/actions/loginActions";
-import { signOut, auth } from "../../firebase";
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-const drawerWidth = 240;
+import MuiAppBar from '@mui/material/AppBar'
+import { styled } from '@mui/material/styles'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useMediaQuery } from '@mui/material'
+//imports
+import { signOut, auth } from '../../firebase'
+import { resetAuthValues } from '../../redux/actions/loginActions'
+
+const drawerWidth = 240
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
-}));
+}))
 
 export function useHeader() {
-  const { pathname } = useLocation();
-  const [title, setTitle] = useState("");
-  const [open, setOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const dispatch = useDispatch();
-  const matches = useMediaQuery("(min-width: 600px)");
-  const tabMode = useMediaQuery("(max-width: 900px)");
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user } = useSelector((state) => state?.Auth) ?? {}
+
+  const matches = useMediaQuery('(min-width: 600px)')
+  const tabMode = useMediaQuery('(max-width: 900px)')
+
+  const [title, setTitle] = useState('')
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [showDateModal, setShowDateModal] = React.useState(false)
+
+  const [dateRange, setDateRange] = React.useState({
+    startDate: null,
+    endDate: null,
+  })
 
   const handleOpenMenu = (event) => {
-    setOpen(true);
-    setAnchorEl(event.currentTarget);
-  };
+    setOpen(true)
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const hanldeLogout = () => {
-    signOut(auth);
-    dispatch(resetAuthValues());
-  };
+    signOut(auth)
+    dispatch(resetAuthValues())
+  }
+
+  const onGotoSettingPage = () => navigate('/account-settings')
 
   useEffect(() => {
     setTitle(
-      pathname === "/dashboard"
-        ? "Dashboard"
-        : pathname === "/learn"
-        ? "Learn"
-        : pathname === "/health"
-        ? "Health"
-        : pathname === "/events"
-        ? "Events"
-        : pathname === "/information"
-        ? "Information"
-        : pathname === "/assesment"
-        ? "Assessment"
-        : pathname === "/admins"
-        ? "Admins"
-        : pathname === "/account-settings"
-        ? "Account Settings"
-        : ""
-    );
-  }, [pathname]);
+      pathname === '/dashboard'
+        ? 'Dashboard'
+        : pathname === '/learn'
+        ? 'Learn'
+        : pathname === '/health'
+        ? 'Health'
+        : pathname === '/events'
+        ? 'Events'
+        : pathname === '/information'
+        ? 'Information'
+        : pathname === '/assesment'
+        ? 'Assessment'
+        : pathname === '/admins'
+        ? 'Admins'
+        : pathname === '/account-settings'
+        ? 'Account Settings'
+        : ''
+    )
+  }, [pathname])
   return {
-    title: title,
-    drawerWidth,
+    user,
+    open,
     AppBar,
-    dispatch,
     matches,
     tabMode,
-    handleOpenMenu,
-    open,
     setOpen,
+    dispatch,
     anchorEl,
+    dateRange,
     handleClose,
+    title: title,
     hanldeLogout,
-  };
+    drawerWidth,
+    setDateRange,
+    showDateModal,
+    handleOpenMenu,
+    setShowDateModal,
+    onGotoSettingPage,
+  }
 }
