14b4fa54f3c293a633ec17dd8e661f62473e33f0
sami-chopdawg <sami@chopdawg.com>
2023-09-26 18:08:59 +0500

    feat:files updated

--- src/views/Dasboard/components/DashboardListing/index.js
+++ src/views/Dasboard/components/DashboardListing/index.js
@@ -1,45 +1,45 @@
-import React from "react";
-import { Avatar, CircularProgress, Grid, Typography } from "@mui/material";
-import MedicineIcon from "../../../../assets/icons/medicine.svg";
+import React from 'react'
+import { Avatar, CircularProgress, Grid, Typography } from '@mui/material'
+import MedicineIcon from '../../../../assets/icons/medicine.svg'
 
 export const ListItem = ({ item, listing }) => {
-  const { image, title, clicks } = item;
+  const { image, title, clicks } = item
   return (
     <>
-      <Grid container justifyContent="space-between" alignItems={"center"}>
+      <Grid container justifyContent="space-between" alignItems={'center'}>
         <Grid item xs={8}>
           <Grid
             container
-            alignItems={"center"}
+            alignItems={'center'}
             columnGap={1}
-            flexWrap={"nowrap"}
+            flexWrap={'nowrap'}
           >
             <Grid item>
               <Grid container className="icon-container">
                 <Avatar
                   variant="square"
-                  src={image ?? ""}
-                  className={listing === "events" ? "image-icon" : "icon"}
+                  src={image ?? ''}
+                  className={listing === 'events' ? 'image-icon' : 'icon'}
                 />
               </Grid>
             </Grid>
-            <Grid item sx={{ overflow: "hidden" }}>
+            <Grid item sx={{ overflow: 'hidden' }}>
               <Typography className="title">{title}</Typography>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={4}>
           <Typography className="count">
-            {clicks ? `${clicks} clicks` : "123 clicks"}
+            {clicks ? `${clicks} clicks` : '123 clicks'}
           </Typography>
         </Grid>
       </Grid>
-      <Grid container justifyContent={"center"} my={2}>
+      <Grid container justifyContent={'center'} my={2}>
         <Grid item xs={9} className="item-divider"></Grid>
       </Grid>
     </>
-  );
-};
+  )
+}
 
 export default function DashboardListing({ list, tabs, listing, isLoading }) {
   return (
@@ -47,10 +47,10 @@ export default function DashboardListing({ list, tabs, listing, isLoading }) {
       <Grid container mb={2} px={3}>
         {tabs}
       </Grid>
-      {listing !== "surveys" ? (
+      {listing !== 'surveys' ? (
         <Grid container className="dashboard-listing">
           {isLoading
-            ? "Loading"
+            ? 'Loading'
             : list?.map((item, index) => (
                 <Grid item xs={12} className="dashboard-list-item" key={index}>
                   <ListItem item={item} listing={listing} />
@@ -58,7 +58,7 @@ export default function DashboardListing({ list, tabs, listing, isLoading }) {
               ))}
         </Grid>
       ) : (
-        <Grid container justifyContent={"center"} className="dashboard-listing">
+        <Grid container justifyContent={'center'} className="dashboard-listing">
           {isLoading ? (
             <CircularProgress />
           ) : (
@@ -69,8 +69,8 @@ export default function DashboardListing({ list, tabs, listing, isLoading }) {
               columnSpacing={2}
               rowSpacing={2}
               mt={1}
-              overflowY={"scroll"}
-              sx={{ maxHeight: "240px" }}
+              overflowY={'scroll'}
+              sx={{ maxHeight: '240px' }}
             >
               {list?.map((item, index) => (
                 <Grid item xs={12} sm={list?.length > 2 ? 6 : 12}>
@@ -81,21 +81,21 @@ export default function DashboardListing({ list, tabs, listing, isLoading }) {
                           variant="square"
                           className="icon"
                           src={MedicineIcon}
-                          sx={{ borderRadius: "0px" }}
+                          sx={{ borderRadius: '0px' }}
                         />
                       </Grid>
                     </Grid>
                     <Grid item flexGrow={1} ml={2}>
                       <Grid
                         container
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}
                       >
-                        <Typography className="title">{"351"}</Typography>
+                        <Typography className="title">{'351'}</Typography>
                         <Typography className="count">{item?.text}</Typography>
                       </Grid>
                     </Grid>
-                  </Grid>{" "}
+                  </Grid>
                 </Grid>
               ))}
             </Grid>
@@ -103,5 +103,5 @@ export default function DashboardListing({ list, tabs, listing, isLoading }) {
         </Grid>
       )}
     </Grid>
-  );
+  )
 }
