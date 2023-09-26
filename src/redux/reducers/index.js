2e84153299f466615ac4869b512b42a75b9e529c
sami-chopdawg <sami@chopdawg.com>
2023-09-26 18:07:32 +0500

    feat:added Dashboard to root-reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
-import AuthReducer from "./AuthReducer";
-import SidebarReducer from "./SidebarReducer";
+import AuthReducer from './AuthReducer'
+import SidebarReducer from './SidebarReducer'
+import DashboardReducer from './DashboardReducer'
 
 export const rootReducer = {
   Auth: AuthReducer,
   Sidebar: SidebarReducer,
-};
+  Dashboard: DashboardReducer,
+}
