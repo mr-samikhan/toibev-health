45bfc094ef0487afa3290e243d5a31e7cfbd21a5
sami-chopdawg <sami@chopdawg.com>
2023-09-27 16:52:34 +0500

    feat:date utils

--- src/utils/dateFormats.js
+++ src/utils/dateFormats.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 export const convertToShortDate = (date) => {
   if (date === null || '') {
     return 'Enter Date'
@@ -10,4 +12,41 @@ export const convertToShortDate = (date) => {
     const formattedStartDate = date?.toLocaleDateString('en-US', options)
     return formattedStartDate
   }
+}
+
+//add time to your timestamp
+export const ADD_TIME_TO_TIMESTAMP = (startDate, time) => {
+  const startDateMoment = moment(startDate)
+
+  const timeMoment = moment(time, 'HH:mm')
+
+  const timestamp = startDateMoment.add({
+    hours: timeMoment.hours(),
+    minutes: timeMoment.minutes(),
+  })
+
+  return timestamp.toDate()
+}
+
+//get month year and day from timestamp
+export const getDayYearMonthFromDate = (date) => {
+  const timestamp = date.toDate()
+  const day = timestamp.getDate() // Get the day of the month (1-31)
+  const year = timestamp.getFullYear() // Get the year (e.g., 2023)
+  const month = timestamp.getMonth() + 1 // Get the month (0-11), so add 1 to get (1-12)
+
+  return { day, year, month }
+}
+
+export function convertToObjectToTimestamp(obj) {
+  const dateComponents = obj
+
+  const date = new Date(
+    dateComponents.year,
+    dateComponents.month - 1,
+    dateComponents.day
+  )
+
+  const timestamp = date.getTime()
+  return new Date(timestamp)
 }
