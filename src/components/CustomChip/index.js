142b3e564cfa0cb584239d2de5a35a97be473d9b
sami-chopdawg <sami@chopdawg.com>
2023-10-04 17:56:59 +0500

    feat:code updated

--- src/components/CustomChip/index.js
+++ src/components/CustomChip/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Chip } from "@mui/material";
-import { ReactComponent as CloseIcon } from "../../assets/icons/close-circle.svg";
-import "./style.scss";
+import React from 'react'
+import { Chip } from '@mui/material'
+import { ReactComponent as CloseIcon } from '../../assets/icons/close-circle.svg'
+import './style.scss'
 
 export const CustomChip = ({ index, title, handleDelete }) => {
   return (
     <Chip
-      className="chip"
       label={title}
-      onDelete={() => handleDelete(index)}
+      className="chip"
       deleteIcon={<CloseIcon />}
+      onDelete={() => handleDelete(index)}
     />
-  );
-};
+  )
+}
