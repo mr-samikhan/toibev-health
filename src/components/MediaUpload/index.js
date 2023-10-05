df7d70a3f5d9274b64b2a138d30ba20a194ede26
sami-chopdawg <sami@chopdawg.com>
2023-10-05 22:35:03 +0500

    feat:Added new props

--- src/components/MediaUpload/index.js
+++ src/components/MediaUpload/index.js
@@ -1,26 +1,30 @@
-import React, { useRef } from "react";
-import { Grid, IconButton, useMediaQuery, Typography } from "@mui/material";
-import { ReactComponent as ClearIcon } from "../../assets/icons/clear.svg";
-import icons from "../../assets";
-import "./style.scss";
+import React, { useRef } from 'react'
+import { Grid, IconButton, useMediaQuery, Typography } from '@mui/material'
+
+//imports
+import './style.scss'
+import icons from '../../assets'
+import { ReactComponent as ClearIcon } from '../../assets/icons/clear.svg'
 
 const ImageUploader = ({
-  fileType = "image",
+  title,
+  onChange,
   selectedFile,
   setSelectedFile,
-  title,
+  fileType = 'image',
+  handleImageRemove,
 }) => {
-  const mobile = useMediaQuery("(max-width: 600px)");
-  const fileInputRef = useRef(null);
+  const mobile = useMediaQuery('(max-width: 600px)')
+  const fileInputRef = useRef(null)
 
   const handleFileUpload = () => {
-    fileInputRef.current.click();
-  };
+    fileInputRef.current.click()
+  }
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
+    const file = event.target.files[0]
     if (file && isFileTypeAllowed(file)) {
-      const fileUrl = URL.createObjectURL(file);
+      const fileUrl = URL.createObjectURL(file)
 
       setSelectedFile({
         ...selectedFile,
@@ -29,47 +33,47 @@ const ImageUploader = ({
         fileSize: `${(file.size / (1024 * 1024)).toFixed(2)}mb`,
         fileType: file.type,
         fileUrl,
-      });
+      })
     }
-  };
+  }
 
   const removeSelectedFile = (event) => {
-    event.stopPropagation();
-    setSelectedFile(null);
-  };
+    event.stopPropagation()
+    setSelectedFile(null)
+  }
 
   const isFileTypeAllowed = (file) => {
-    const allowedTypes = getFileAcceptValue(fileType).split(",");
-    const fileTypeRegex = new RegExp(allowedTypes.join("|"), "i");
-    return fileTypeRegex.test(file.type);
-  };
+    const allowedTypes = getFileAcceptValue(fileType).split(',')
+    const fileTypeRegex = new RegExp(allowedTypes.join('|'), 'i')
+    return fileTypeRegex.test(file.type)
+  }
 
   const getFileAcceptValue = () => {
     switch (fileType) {
-      case "image":
-        return "image/*";
-      case "video":
-        return "video/*";
-      case "audio":
-        return "audio/*";
+      case 'image':
+        return 'image/*'
+      case 'video':
+        return 'video/*'
+      case 'audio':
+        return 'audio/*'
       default:
-        return "application/pdf";
+        return 'application/pdf'
     }
-  };
+  }
 
   return (
     <>
       <Typography
         variant="h6"
         mb={{ xs: 1, sm: 2 }}
-        sx={{ fontSize: mobile && "14px" }}
+        sx={{ fontSize: mobile && '14px' }}
       >
         {title}
       </Typography>
       <Grid
         className="media-upload-container"
         onClick={() =>
-          (fileType === "pdf" || !selectedFile?.fileUrl) && handleFileUpload()
+          (fileType === 'pdf' || !selectedFile?.fileUrl) && handleFileUpload()
         }
         container
         direction="column"
@@ -80,19 +84,19 @@ const ImageUploader = ({
             <Grid
               component="span"
               className="remove-image"
-              onClick={removeSelectedFile}
+              onClick={handleImageRemove || removeSelectedFile}
             >
               <IconButton>
                 <ClearIcon />
               </IconButton>
             </Grid>
-            {fileType === "image" ? (
+            {fileType === 'image' ? (
               <img
                 src={selectedFile.fileUrl}
                 alt="Uploaded"
                 className="--image"
               />
-            ) : fileType === "video" ? (
+            ) : fileType === 'video' ? (
               <video controls className="--video">
                 <source
                   src={selectedFile.fileUrl}
@@ -122,14 +126,14 @@ const ImageUploader = ({
           <>
             <Grid component="span" className="icon" mb={2}>
               <img
-                src={fileType === "pdf" ? icons.pdfIcon : icons.mediaUploadIcon}
+                src={fileType === 'pdf' ? icons.pdfIcon : icons.mediaUploadIcon}
                 alt="Upload Icon"
                 className="icon-image"
               />
             </Grid>
 
             <Grid component="span" className="text">
-              {`Click to upload ${!mobile ? "or drop a file" : ""}`}
+              {`Click to upload ${!mobile ? 'or drop a file' : ''}`}
             </Grid>
           </>
         )}
@@ -137,13 +141,13 @@ const ImageUploader = ({
           ref={fileInputRef}
           type="file"
           accept={getFileAcceptValue()}
-          style={{ display: "none" }}
-          onChange={handleFileChange}
+          style={{ display: 'none' }}
+          onChange={onChange || handleFileChange}
           onClick={(e) => (e.target.value = null)}
         />
       </Grid>
     </>
-  );
-};
+  )
+}
 
-export default ImageUploader;
+export default ImageUploader
