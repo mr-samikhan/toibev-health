0b7fc1fab895a482befa5305e408bd8694e1fc8c
sami-chopdawg <sami@chopdawg.com>
2023-10-05 22:37:24 +0500

    feat:added UI work and thumbnail work for multiple-videos upload

--- src/views/Learn/components/Forms/LanguageForm/index.js
+++ src/views/Learn/components/Forms/LanguageForm/index.js
@@ -3,6 +3,7 @@ import { Controller } from 'react-hook-form'
 import { Grid, Box, Typography } from '@mui/material'
 
 //imports
+import { createThumbnailFromVideo } from '../../../actions'
 import useLanguageForm from '../../../hook/useLanguageForm'
 import CustomButton from '../../../../../components/CustomButton'
 import ImageUploader from '../../../../../components/MediaUpload'
@@ -26,11 +27,34 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
     isLoadingCultures,
     isFetchingCultures,
     onHandleDeleteTribe,
+    //
+    setSelectedVideos,
+    selectedVideos,
   } = useLanguageForm({
     isEdit,
     initialState,
     setOpen,
   })
+
+  const onChange = (e) => {
+    const files = e.target.files
+
+    if (files && files.length > 0) {
+      const newImages = Array.from(files).map((file) =>
+        createThumbnailFromVideo(file)
+      )
+
+      Promise.all(newImages).then((resolvedImages) => {
+        setSelectedVideos([...selectedVideos, ...resolvedImages])
+      })
+    }
+  }
+
+  const handleImageRemove = (indexToRemove) => {
+    const updatedSelectedVideos = [...selectedVideos]
+    updatedSelectedVideos.splice(indexToRemove, 1)
+    setSelectedVideos(updatedSelectedVideos)
+  }
 
   return (
     <Box component="form" onSubmit={handleSubmit(onSubmit)}>
@@ -112,6 +136,7 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
               >
                 <Grid item xs={6}>
                   <ImageUploader
+                    // title="Image"
                     fileType="image"
                     selectedFile={selectedImage}
                     setSelectedFile={setSelectedImage}
@@ -120,6 +145,29 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
               </Grid>
             </Grid>
           </Grid>
+        </Grid>
+        <Grid item container md={12} spacing={2} mb={2}>
+          {selectedVideos?.map((item, index) => (
+            <Grid item xs={6} md={6} key={index}>
+              <ImageUploader
+                fileType="video"
+                // title="Videos"
+                selectedFile={item}
+                handleImageRemove={() => handleImageRemove(index)}
+              />
+            </Grid>
+          ))}
+          {selectedVideos.length < 8 && (
+            <Grid item xs={6} md={6} mb={2}>
+              <ImageUploader
+                fileType="video"
+                // title="Videos"
+                // selectedFile={''}
+                setSelectedFile={setSelectedVideos}
+                onChange={(e) => onChange(e)}
+              />
+            </Grid>
+          )}
         </Grid>
         <Grid item xs={12} mb={isEdit && 2}>
           <CustomButton variant="contained" type="submit">
