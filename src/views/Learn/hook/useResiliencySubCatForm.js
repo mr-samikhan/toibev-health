dd61ea72af0e74e8a4c2ab267dd7a210828a1cbf
sami-chopdawg <sami@chopdawg.com>
2023-10-06 22:17:38 +0500

    fix:all issues fixed in Resiliency Tab

--- src/views/Learn/hook/useResiliencySubCatForm.js
+++ src/views/Learn/hook/useResiliencySubCatForm.js
@@ -1,46 +1,49 @@
-import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "react-query";
-import { addResiliencySubCat, updateResiliencySubCat } from "../actions";
+import React, { useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from 'react-query'
+import { addResiliencySubCat, updateResiliencySubCat } from '../actions'
 
 export default function useResiliencySubCatForm({
-  isEdit,
-  initialState,
-  setOpen,
   cat,
+  isEdit,
+  setOpen,
+  initialState,
 }) {
   const { control, handleSubmit, reset } = useForm({
     defaultValues: { title: initialState?.title },
-  });
+  })
 
   const [pdf, setPdf] = React.useState(
-    {
-      fileName: initialState?.pdf?.fileName,
-      fileSize: initialState?.pdf?.fileSize,
-    } ?? null
-  );
+    isEdit
+      ? {
+          fileUrl: initialState?.pdf?.fileUrl || '',
+          fileName: initialState?.pdf?.fileName || '',
+          fileSize: initialState?.pdf?.fileSize || '',
+        }
+      : null
+  )
   const [image, setImage] = React.useState({
-    fileUrl: initialState?.cover_img || "",
-  });
-  const pdfInputRef = useRef(null);
-  const queryClient = useQueryClient();
+    fileUrl: initialState?.cover_img || '',
+  })
+  const pdfInputRef = useRef(null)
+  const queryClient = useQueryClient()
 
   const { isLoading, mutate } = useMutation(
     isEdit ? updateResiliencySubCat : addResiliencySubCat,
     {
       onSuccess: (success) => {
-        setOpen(false);
-        queryClient.invalidateQueries("get-reseliency");
+        setOpen(false)
+        queryClient.invalidateQueries('get-reseliency')
       },
       onError: (error) => {
-        console.log(error);
+        console.log(error)
       },
     }
-  );
+  )
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    const fileUrl = URL.createObjectURL(file);
+    const file = event.target.files[0]
+    const fileUrl = URL.createObjectURL(file)
 
     setPdf({
       file,
@@ -48,40 +51,41 @@ export default function useResiliencySubCatForm({
       fileSize: `${(file.size / (1024 * 1024)).toFixed(2)}MB`,
       fileType: file.type,
       fileUrl,
-    });
-  };
+    })
+  }
 
   const handleFileUpload = () => {
-    pdfInputRef.current.click();
-  };
+    pdfInputRef.current.click()
+  }
 
   const handleRemoveFile = () => {
-    setPdf(null);
-  };
+    setPdf(null)
+  }
 
   const onSubmit = (formData) => {
     const data = {
       ...formData,
       pdf,
-      image,
-      cat: cat ?? initialState?.cat,
-    };
-    console.log(data);
-    isEdit ? mutate({ ...data, id: initialState?.id }) : mutate(data);
-  };
+      cover_img: image,
+      cat: cat || '',
+    }
+    isEdit
+      ? mutate({ data: { ...data, id: initialState?.id }, collectionName: cat })
+      : mutate(data)
+  }
 
   return {
+    pdf,
+    image,
+    setPdf,
     control,
-    handleSubmit,
+    setImage,
     onSubmit,
     isLoading,
-    pdf,
-    setPdf,
-    handleFileChange,
     pdfInputRef,
+    handleSubmit,
     handleFileUpload,
     handleRemoveFile,
-    image,
-    setImage,
-  };
+    handleFileChange,
+  }
 }
