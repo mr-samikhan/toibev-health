dab54ceed47d4e88cd374f1c7333337a6688eddb
sami-chopdawg <sami@chopdawg.com>
2023-10-09 18:10:31 +0500

    feat:code modified

--- src/hooks/useGetTreatmentOptions.js
+++ src/hooks/useGetTreatmentOptions.js
@@ -1,36 +1,36 @@
-import { useQuery } from "react-query";
-import { firestore, collection, getDocs } from "../firebase";
+import { useQuery } from 'react-query'
+import { firestore, collection, getDocs } from '../firebase'
 
 const fetchInfo = async (id) => {
-  let data = [];
+  let data = []
   try {
     const querySnapshot = await getDocs(
-      collection(firestore, "Treatment", "general", "list", id, "options")
-    );
+      collection(firestore, 'Treatment', 'general', 'list', id, 'options')
+    )
 
     querySnapshot.forEach((document) => {
       let treatment = {
         id: document.id,
         ...document.data(),
-      };
-      data.push(treatment);
-    });
+      }
+      data.push(treatment)
+    })
 
-    return data;
+    return data
   } catch (error) {
-    return error;
+    return error
   }
-};
+}
 
 export const useGetTreatmentOptions = ({ enabled = true, id }) => {
   const { data, isLoading, error, isFetching } = useQuery(
-    ["get-all-treatment-options"],
+    ['get-all-treatment-options'],
     () => fetchInfo(id),
     {
       enabled,
       refetchOnWindowFocus: false,
     }
-  );
+  )
 
-  return { isLoading, error, data, isFetching };
-};
+  return { isLoading, error, data, isFetching }
+}
