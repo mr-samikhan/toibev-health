3037b72aaf10b8b8cfe143d73d0886df3345d26b
sami-chopdawg <sami@chopdawg.com>
2023-10-10 15:51:00 +0500

    feat:code modified

--- src/components/CustomTextfield/index.js
+++ src/components/CustomTextfield/index.js
@@ -1,87 +1,87 @@
-import React from "react";
-import { TextField, InputAdornment, IconButton } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MenuItem from "@mui/material/MenuItem";
-import { styled } from "@mui/material/styles";
+import React from 'react'
+import MenuItem from '@mui/material/MenuItem'
+import { styled } from '@mui/material/styles'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { TextField, InputAdornment, IconButton } from '@mui/material'
 
 export const CustomIconButton = styled((props) => <IconButton {...props} />)(
   ({ theme }) => ({
-    "&.MuiIconButton-root": {
-      background: "rgba(242, 106, 71, 0.08)",
-      borderRadius: "10px",
-      marginRight: "8px",
+    '&.MuiIconButton-root': {
+      background: 'rgba(242, 106, 71, 0.08)',
+      borderRadius: '10px',
+      marginRight: '8px',
       filter:
-        "invert(50%) sepia(97%) saturate(1979%) hue-rotate(334deg) brightness(102%) contrast(90%)",
+        'invert(50%) sepia(97%) saturate(1979%) hue-rotate(334deg) brightness(102%) contrast(90%)',
     },
-    "& svg": {
-      width: "16px",
-      height: "16px",
+    '& svg': {
+      width: '16px',
+      height: '16px',
     },
   })
-);
+)
 
 const CustomTextField = styled((props) => <TextField focused {...props} />)(
   ({ theme }) => ({
-    "&.MuiTextField-root": {
-      "& input": {
-        width: "unset",
+    '&.MuiTextField-root': {
+      '& input': {
+        width: 'unset',
         flexGrow: 1,
       },
-      "& .MuiFormLabel-root": { color: "#000" },
-      "& .MuiOutlinedInput-notchedOutline": {
-        border: "1px solid #DCDCDC",
-        borderRadius: "14px",
+      '& .MuiFormLabel-root': { color: '#000' },
+      '& .MuiOutlinedInput-notchedOutline': {
+        border: '1px solid #DCDCDC',
+        borderRadius: '14px',
       },
       transition: theme.transitions.create([
-        "border-color",
-        "background-color",
-        "box-shadow",
+        'border-color',
+        'background-color',
+        'box-shadow',
       ]),
-      "&:hover": {
-        backgroundColor: "transparent",
+      '&:hover': {
+        backgroundColor: 'transparent',
       },
-      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+      '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
         // borderColor: theme.palette.primary.main,
         // borderWidth: "2px",
-        border: "1px solid #DCDCDC",
-        borderRadius: "14px",
+        border: '1px solid #DCDCDC',
+        borderRadius: '14px',
       },
-      "& fieldset": {
-        padding: "0px 24px",
+      '& fieldset': {
+        padding: '0px 24px',
       },
-      "& label": { left: "16px" },
-      "& input": { paddingLeft: "24px" },
+      '& label': { left: '16px' },
+      '& input': { paddingLeft: '24px' },
     },
   })
-);
+)
 
 export default function CustomTextfield({
-  EndIcon,
-  EndIconPrimary,
-  noBackground,
-  select,
-  options,
-  errorMessage,
-  multiline,
-  handleEnterKeyPress,
   sx,
   ref,
+  select,
+  EndIcon,
+  options,
+  multiline,
+  noBackground,
+  errorMessage,
+  EndIconPrimary,
+  handleEnterKeyPress,
   ...rest
 }) {
   return (
     <CustomTextField
+      fullWidth
       id="outlined"
       select={select}
-      fullWidth
       multiline={multiline}
       helperText={errorMessage}
       onKeyPress={handleEnterKeyPress}
       sx={{
-        "& .MuiInputBase-input": {
-          padding: "16px",
+        '& .MuiInputBase-input': {
+          padding: '16px',
         },
-        "& .MuiInputBase-root": {
-          alignItems: multiline ? "start" : "center",
+        '& .MuiInputBase-root': {
+          alignItems: multiline ? 'start' : 'center',
         },
         ...sx,
       }}
@@ -90,7 +90,7 @@ export default function CustomTextfield({
           EndIconPrimary || EndIcon ? (
             <InputAdornment
               position="end"
-              sx={{ marginTop: multiline && "28px" }}
+              sx={{ marginTop: multiline && '28px' }}
             >
               {EndIconPrimary ? (
                 <EndIconPrimary />
@@ -120,5 +120,5 @@ export default function CustomTextfield({
           </MenuItem>
         ))}
     </CustomTextField>
-  );
+  )
 }
