5000c3b304c483f47ee3c8aff62cb01524b83aa8
sami-chopdawg <sami@chopdawg.com>
2023-10-17 17:24:08 +0500

    feat:code modified

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import App from "./App";
-import "./index.scss";
+import React from 'react'
+import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@mui/material'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+//imports
+import './index.scss'
+import App from './App'
+import { theme } from './theme'
+import { persistor, store } from './redux/store'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Router>
-            <App />
-          </Router>
-        </PersistGate>
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>
-);
+  // <React.StrictMode>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </ThemeProvider>
+  </Provider>
+  // </React.StrictMode>
+)
