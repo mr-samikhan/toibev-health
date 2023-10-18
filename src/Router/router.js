70d402fe3fdcfeac2712253b1ba8a565e95c4bb9
sami-chopdawg <sami@chopdawg.com>
2023-10-18 16:09:11 +0500

    feat:added new route for home-screen

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,21 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import { Learn } from "../views/Learn";
-import { Admins } from "../views/Admins";
-import { Assessment } from "../views/Assessment";
-import { Health } from "../views/Health";
-import { Events } from "../views/Events";
-import ProtectedRoutes from "../HOC/ProtectedRoutes";
-import { Information } from "../views/Information";
-import Layout from "../Layout";
-import Settings from "../views/Setttings";
-import SingleAssessment from "../views/Assessment/SingleAssessment";
-import Login from "../views/Login";
-import { Dashboard } from "../views/Dasboard/Dashboard";
-import ResetPassword from "../views/ResetPassword";
-import { useLocation } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
+//imports
+import Layout from '../Layout'
+import Login from '../views/Login'
+import { Learn } from '../views/Learn'
+import { Admins } from '../views/Admins'
+import { Health } from '../views/Health'
+import { Events } from '../views/Events'
+import Settings from '../views/Setttings'
+import { Assessment } from '../views/Assessment'
+import { Information } from '../views/Information'
+import ResetPassword from '../views/ResetPassword'
+import ProtectedRoutes from '../HOC/ProtectedRoutes'
+import { Dashboard } from '../views/Dasboard/Dashboard'
+import SingleAssessment from '../views/Assessment/SingleAssessment'
 
 export function Router() {
-  const { search } = useLocation();
   return (
     <>
       <Routes>
@@ -25,20 +24,21 @@ export function Router() {
       <ProtectedRoutes>
         <Routes>
           <Route path="/" exact element={<Layout />}>
-            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/home" exact element={<Events />} />
             <Route path="/learn" exact element={<Learn />} />
             <Route path="/health" exact element={<Health />} />
             <Route path="/events" exact element={<Events />} />
+            <Route path="/admins" exact element={<Admins />} />
+            <Route path="/dashboard" exact element={<Dashboard />} />
             <Route path="/information" exact element={<Information />} />
+            <Route path="/account-settings" exact element={<Settings />} />
             <Route path="/assesment">
               <Route path="/assesment" exact element={<Assessment />} />
               <Route path="/assesment/:id" element={<SingleAssessment />} />
             </Route>
-            <Route path="/admins" exact element={<Admins />} />
-            <Route path="/account-settings" exact element={<Settings />} />
           </Route>
-        </Routes>{" "}
+        </Routes>
       </ProtectedRoutes>
     </>
-  );
+  )
 }
