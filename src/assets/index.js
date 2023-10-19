e79d9f2c8f3d003cc14bb72b9850be6c89d7c884
sami-chopdawg <sami@chopdawg.com>
2023-10-19 17:26:35 +0500

    feat:file modified

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,33 +1,33 @@
-import peopleIcon from "./icons/people.svg";
-import languageIcon from "./icons/language.svg";
-import clockIcon from "./icons/clock.svg";
-import editIcon from "./icons/edit.svg";
-import documentIcon from "./icons/document.svg";
-import clipboardIcon from "./icons/clipboard.svg";
-import deleteIcon from "./icons/delete.svg";
-import editCalendarIcon from "./icons/edit-calendar.svg";
-import statsCalendarIcon from "./icons/stats-calendar.svg";
-import statsAppointmentIcon from "./icons/stats-appointment.svg";
-import statsUsersIcon from "./icons/stats-users.svg";
-import statsViewsIcon from "./icons/stats-views.svg";
-import mediaUploadIcon from "./icons/media-upload.png";
-import clearIcon from "./icons/clear.svg";
-import pdfIcon from "./icons/pdf.svg";
+import pdfIcon from './icons/pdf.svg'
+import editIcon from './icons/edit.svg'
+import clearIcon from './icons/clear.svg'
+import clockIcon from './icons/clock.svg'
+import peopleIcon from './icons/people.svg'
+import deleteIcon from './icons/delete.svg'
+import languageIcon from './icons/language.svg'
+import documentIcon from './icons/document.svg'
+import clipboardIcon from './icons/clipboard.svg'
+import statsUsersIcon from './icons/stats-users.svg'
+import statsViewsIcon from './icons/stats-views.svg'
+import mediaUploadIcon from './icons/media-upload.png'
+import editCalendarIcon from './icons/edit-calendar.svg'
+import statsCalendarIcon from './icons/stats-calendar.svg'
+import statsAppointmentIcon from './icons/stats-appointment.svg'
 
 export default {
-  peopleIcon,
+  pdfIcon,
   editIcon,
-  languageIcon,
   clockIcon,
+  clearIcon,
+  peopleIcon,
+  deleteIcon,
+  languageIcon,
   documentIcon,
   clipboardIcon,
-  deleteIcon,
+  statsViewsIcon,
+  statsUsersIcon,
+  mediaUploadIcon,
   editCalendarIcon,
   statsCalendarIcon,
   statsAppointmentIcon,
-  statsUsersIcon,
-  statsViewsIcon,
-  mediaUploadIcon,
-  clearIcon,
-  pdfIcon,
-};
+}
