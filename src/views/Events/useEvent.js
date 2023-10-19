b67f6eec36d3db80b94748ee0057293e3b805a3e
sami-chopdawg <sami@chopdawg.com>
2023-10-19 15:48:23 +0500

    feat:added service for updating event status

--- src/views/Events/useEvent.js
+++ src/views/Events/useEvent.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
-import { useGetEvents } from "../../hooks/useGetEvents";
+import { useState } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+//imports
+import { updateEvent } from './actions'
+import { useGetEvents } from '../../hooks/useGetEvents'
 
 export default function useEvent() {
-  const [open, setOpen] = useState(false);
-  const { data, isLoading, isFetching } = useGetEvents({});
+  const queryClient = useQueryClient()
+
+  const [open, setOpen] = useState(false)
+  const { data, isLoading, isFetching } = useGetEvents({})
+
+  const { mutate: updateEventDoc } = useMutation(updateEvent, {
+    onSuccess: (success) => {
+      setOpen(false)
+      queryClient.invalidateQueries('get-all-events')
+    },
+    onError: (error) => {
+      console.log(error)
+    },
+  })
+
   return {
     open,
     setOpen,
     data,
     isLoading,
     isFetching,
-  };
+    updateEventDoc,
+  }
 }
