36a7b4a32d6aaf8774fb7d15c7c6d62fbdc54126
sami-chopdawg <sami@chopdawg.com>
2023-10-20 20:08:12 +0500

    feat:added alerts to resillenceForm

--- src/views/Learn/hook/useResilienceForm.js
+++ src/views/Learn/hook/useResilienceForm.js
@@ -1,26 +1,56 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "react-query";
-import { addResiliency, deleteLanguage, updateLanguage } from "../actions";
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { useMutation, useQueryClient } from 'react-query'
+import { getErrorMessage } from '../../Login/utils'
+import { addResiliency, updateLanguage } from '../actions'
+import { setAlertValues } from '../../../redux/actions/loginActions'
 
 export default function useResilienceForm({ isEdit, initialState, setOpen }) {
   const { control, handleSubmit } = useForm({
     defaultValues: { title: initialState?.title },
-  });
-  const queryClient = useQueryClient();
+  })
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
+
+  //success
+  const onSuccess = ({ isDelete }) => {
+    dispatch(
+      setAlertValues({
+        type: 'success',
+        message: isDelete
+          ? 'Resilliency deleted successfully'
+          : isEdit
+          ? 'Resilliency updated successfully'
+          : 'Resilliency added successfully',
+        isOpen: true,
+      })
+    )
+
+    setTimeout(() => {
+      setOpen(false)
+      queryClient.invalidateQueries('get-reseliency')
+    }, 3000)
+  }
+
+  //error
+  const onError = (error) => {
+    const err = getErrorMessage(error)
+    dispatch(
+      setAlertValues({
+        type: 'error',
+        isOpen: true,
+        message: err || 'Something went wrong!',
+      })
+    )
+  }
 
   const { isLoading, mutate } = useMutation(
     isEdit ? updateLanguage : addResiliency,
     {
-      onSuccess: (success) => {
-        setOpen(false);
-        queryClient.invalidateQueries("get-reseliency");
-      },
-      onError: (error) => {
-        console.log(error);
-      },
+      onSuccess: (success) => onSuccess({ isDelete: false }),
+      onError: (error) => onError(error),
     }
-  );
+  )
 
   const onSubmit = (formData) => {
     const data = {
@@ -29,21 +59,21 @@ export default function useResilienceForm({ isEdit, initialState, setOpen }) {
         ...(initialState?.menu || []),
         {
           title: formData.title,
-          value: formData.title?.split(" ").join("_").toLowerCase(),
+          value: formData.title?.split(' ').join('_').toLowerCase(),
         },
       ],
-    };
-    if (isEdit) {
-      mutate({ ...data, id: initialState.id });
-    } else {
-      mutate(data);
     }
-  };
+    if (isEdit) {
+      mutate({ ...data, id: initialState.id })
+    } else {
+      mutate(data)
+    }
+  }
 
   return {
     control,
     handleSubmit,
     onSubmit,
     isLoading,
-  };
+  }
 }
