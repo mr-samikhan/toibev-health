fea6b0ff9af3535b7f7dd9a1bab0b349a15cdf5a
sami-chopdawg <sami@chopdawg.com>
2023-10-20 20:07:25 +0500

    feat:added success and error alerts

--- src/views/Health/hooks/useMedicationForm.js
+++ src/views/Health/hooks/useMedicationForm.js
@@ -1,57 +1,84 @@
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "react-query";
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from 'react-query'
 import {
   addMedicationsList,
   deleteMedicationsList,
   updateMedicationsList,
-} from "../actions";
+} from '../actions'
+import { getErrorMessage } from '../../Login/utils'
+import { setAlertValues } from '../../../redux/actions/loginActions'
 
 export default function useMedicationForm({ isEdit, initialState, setOpen }) {
-  const { control, handleSubmit, reset } = useForm();
-  const queryClient = useQueryClient();
+  const { control, handleSubmit, reset } = useForm()
+
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
+
+  //success
+  const onSuccess = ({ isDelete }) => {
+    dispatch(
+      setAlertValues({
+        type: 'success',
+        message: isDelete
+          ? 'Medication deleted successfully'
+          : isEdit
+          ? 'Medication updated successfully'
+          : 'Medication added successfully',
+        isOpen: true,
+      })
+    )
+
+    setTimeout(() => {
+      setOpen(false)
+      queryClient.invalidateQueries('get-all-medication')
+    }, 3000)
+  }
+
+  //error
+  const onError = (error) => {
+    const err = getErrorMessage(error)
+    dispatch(
+      setAlertValues({
+        type: 'error',
+        isOpen: true,
+        message: err || 'Something went wrong!',
+      })
+    )
+  }
 
   const { isLoading, mutate } = useMutation(
     isEdit ? updateMedicationsList : addMedicationsList,
     {
-      onSuccess: (success) => {
-        setOpen(false);
-        queryClient.invalidateQueries("get-all-medication");
-      },
-      onError: (error) => {
-        console.log(error);
-      },
+      onSuccess: (success) => onSuccess({ isDelete: false }),
+      onError: (error) => onError(error),
     }
-  );
+  )
 
   const { isLoading: isLoadingDelete, mutate: mutateDelete } = useMutation(
     deleteMedicationsList,
     {
-      onSuccess: (success) => {
-        setOpen(false);
-        queryClient.invalidateQueries("get-all-medication");
-      },
-      onError: (error) => {
-        console.log(error);
-      },
+      onSuccess: (success) => onSuccess({ isDelete: true }),
+      onError: (error) => onError(error),
     }
-  );
+  )
 
   const onSubmit = (data) => {
     const body = {
       title: data?.title,
       url: data?.url,
-    };
-    isEdit ? mutate({ ...body, id: initialState.id }) : mutate(body);
-  };
+    }
+    isEdit ? mutate({ ...body, id: initialState.id }) : mutate(body)
+  }
 
   const onDelete = () => {
-    mutateDelete(initialState?.id);
-  };
+    mutateDelete(initialState?.id)
+  }
 
   useEffect(() => {
-    isEdit && reset({ ...initialState });
-  }, isEdit);
+    isEdit && reset({ ...initialState })
+  }, isEdit)
 
   return {
     control,
@@ -61,5 +88,5 @@ export default function useMedicationForm({ isEdit, initialState, setOpen }) {
     onDelete,
     isEdit,
     isLoadingDelete,
-  };
+  }
 }
