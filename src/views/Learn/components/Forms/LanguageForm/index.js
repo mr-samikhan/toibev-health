6cc5253c3f4567a8a0c295d4c9c7c55620dea9b8
sami-chopdawg <sami@chopdawg.com>
2023-11-13 17:49:15 +0500

    feat:worked on courses section

--- src/views/Learn/components/Forms/LanguageForm/index.js
+++ src/views/Learn/components/Forms/LanguageForm/index.js
@@ -4,6 +4,8 @@ import { Grid, Box, Typography, IconButton } from '@mui/material'
 
 //imports
 import plus from '../../../../../assets/icons/plus-icon.svg'
+import calendarIcon from '../../../../../assets/icons/events-Icon.svg'
+import locationIcon from '../../../../../assets/icons/location-blue.svg'
 import { createThumbnailFromVideo } from '../../../actions'
 import useLanguageForm from '../../../hook/useLanguageForm'
 import CustomButton from '../../../../../components/CustomButton'
@@ -12,6 +14,9 @@ import { CustomChip } from '../../../../../components/CustomChip'
 import CustomTextfield from '../../../../../components/CustomTextfield'
 import { ReactComponent as PeopleIcon } from '../../../../../assets/icons/people.svg'
 import { MuiCustomAutocomplete } from '../../../../../components/MuiCustomAutocomplete/MuiCustomAutocomplete'
+import AlertDialog from '../../../../../components/AlertDialog'
+import { CustomList } from '../../../../../components/List'
+import { useGetEvents } from '../../../../../hooks/useGetEvents'
 
 export default function LangugaeForm({ isEdit, initialState, setOpen }) {
   const {
@@ -45,6 +50,25 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
   })
 
   const [isCoursesModal, setIsCoursesModal] = React.useState(false)
+  const [selectedCourses, setSelectedCourses] = React.useState([])
+
+  const { data } = useGetEvents({ enabled: true })
+
+  const onSelectCourses = (course) => {
+    let index = selectedCourses.findIndex((item) => item.id === course.id)
+    if (index === -1) {
+      setSelectedCourses([...selectedCourses, course])
+    } else {
+      let temp = [...selectedCourses]
+      temp.splice(index, 1)
+      setSelectedCourses(temp)
+    }
+  }
+
+  const onCancel = () => {
+    setSelectedCourses([])
+    setIsCoursesModal(false)
+  }
 
   const onChange = (e) => {
     const files = e.target.files
@@ -85,6 +109,19 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
     updatedSelectedVideos[indexToRemove][fileType] = ''
     updatedSelectedVideos[indexToRemove].file = ''
     setLanguages(updatedSelectedVideos)
+  }
+
+  const formatData = (data) => {
+    let comingDate = data.toDate()
+    const formattedDate = comingDate.toLocaleString('en-US', {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      // hour: 'numeric',
+      // minute: 'numeric',
+      hour12: true,
+    })
+    return formattedDate
   }
 
   return (
@@ -312,11 +349,98 @@ export default function LangugaeForm({ isEdit, initialState, setOpen }) {
           >
             Courses
           </Typography>
-          <IconButton>
+          <IconButton onClick={() => setIsCoursesModal(true)}>
             <img src={plus} alt="plus" />
           </IconButton>
         </Grid>
-        {isCoursesModal && <Typography>Hello</Typography>}
+        {/* courses modal */}
+        {isCoursesModal && (
+          <AlertDialog
+            title="Add Course"
+            open={isCoursesModal}
+            setOpen={setIsCoursesModal}
+            message={
+              <>
+                <CustomList
+                  list={data}
+                  icon={false}
+                  isNoicon={false}
+                  onRowClick={onSelectCourses}
+                  selectedRows={selectedCourses}
+                />
+                <Grid item xs={12} mb={2}>
+                  <CustomButton
+                    variant="contained"
+                    onClick={() => setIsCoursesModal(false)}
+                  >
+                    {'Add Course'}
+                  </CustomButton>
+                </Grid>
+                <Grid item xs={12} mt={2}>
+                  <CustomButton variant="outlined" onClick={onCancel}>
+                    {'Cancel'}
+                  </CustomButton>
+                </Grid>
+              </>
+            }
+          />
+        )}
+        {/* end */}
+
+        <Grid
+          item
+          xs={12}
+          md={12}
+          mb={3}
+          gap={2}
+          display="flex"
+          flexWrap="wrap"
+          flexDirection="row"
+        >
+          {selectedCourses?.map((course, index) => (
+            <Grid
+              item
+              p={1}
+              md={5.8}
+              mb={3}
+              key={index}
+              borderRadius={4}
+              bgcolor="rgba(59, 125, 125, 0.10)"
+            >
+              <Box width="90%" height={108} m="auto">
+                <img
+                  alt="course"
+                  width="100%"
+                  height="100%"
+                  src={course?.image}
+                  style={{
+                    borderRadius: '8px',
+                  }}
+                />
+              </Box>
+              <Typography
+                mt={1}
+                fontSize={16}
+                fontWeight={700}
+                textAlign="center"
+              >
+                {course.title}
+              </Typography>
+              <Box display="flex" gap={2} justifyContent="center">
+                <img src={calendarIcon} alt="" />
+                <Typography fontSize={14} fontWeight={400}>{`${formatData(
+                  course.startDate
+                )} at ${formatData(course.endDate)}`}</Typography>
+              </Box>
+              <Box display="flex" gap={2} justifyContent="center">
+                <img src={locationIcon} alt="" />
+                <Typography fontSize={14} fontWeight={400}>
+                  {course.location}
+                </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
         <Grid item xs={12} mb={isEdit && 2}>
           <CustomButton variant="contained" type="submit">
             {isLoading ? 'Saving...' : 'Save Language'}
