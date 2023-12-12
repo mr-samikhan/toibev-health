3b454c945b9653bfc681291e6410ba5c99e1f980
sami-chopdawg <sami@chopdawg.com>
2023-12-12 18:52:50 +0500

    fix:TOI-149 to 152 issues has been resolved

--- src/hooks/useGetClinics.js
+++ src/hooks/useGetClinics.js
@@ -1,39 +1,39 @@
-import { useQuery } from "react-query";
-import { firestore, collection, getDocs, doc, getDoc } from "../firebase";
+import { useQuery } from 'react-query'
+import { firestore, collection, getDocs, doc, getDoc } from '../firebase'
 
 const fetchInfo = async () => {
-  let clinicsData = [];
+  let clinicsData = []
   try {
-    const clinics = collection(firestore, "Information", "clinics", "list");
-    const querySnapshot = await getDocs(clinics);
-    const infoRef = collection(firestore, "Information");
-    const docRef = doc(infoRef, "clinics");
-    const docSnap = await getDoc(docRef);
+    const clinics = collection(firestore, 'Information', 'clinics', 'list')
+    const querySnapshot = await getDocs(clinics)
+    const infoRef = collection(firestore, 'Information')
+    const docRef = doc(infoRef, 'clinics')
+    const docSnap = await getDoc(docRef)
 
     querySnapshot.forEach((document) => {
       let service = {
         id: document.id,
         ...document.data(),
-      };
+      }
 
-      clinicsData.push(service);
-    });
-    return { clinicsData, urls: docSnap.data().social_links };
+      clinicsData.push(service)
+    })
+    return { clinicsData, urls: docSnap.data()?.social_links || [] }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 export const useGetClinics = ({ enabled = true }) => {
   const { data, isLoading, error, isFetching } = useQuery(
-    ["get-all-clinics"],
+    ['get-all-clinics'],
     fetchInfo,
     {
       enabled,
       refetchOnWindowFocus: false,
     }
-  );
+  )
 
   return {
     isLoadingClinics: isLoading,
@@ -41,5 +41,5 @@ export const useGetClinics = ({ enabled = true }) => {
     clinics: data?.clinicsData ?? [],
     urls: data?.urls ?? [],
     isFetchingClinics: isFetching,
-  };
-};
+  }
+}
--- src/hooks/useGetServices.js
+++ src/hooks/useGetServices.js
@@ -1,46 +1,46 @@
-import { useQuery } from "react-query";
-import { firestore, collection, getDocs, getDoc, doc } from "../firebase";
+import { useQuery } from 'react-query'
+import { firestore, collection, getDocs, getDoc, doc } from '../firebase'
 
 const fetchInfo = async () => {
-  let servicesData = [];
+  let servicesData = []
   try {
-    const services = collection(firestore, "Information", "services", "list");
-    const infoRef = collection(firestore, "Information");
-    const docRef = doc(infoRef, "services");
-    const docSnap = await getDoc(docRef);
+    const services = collection(firestore, 'Information', 'services', 'list')
+    const infoRef = collection(firestore, 'Information')
+    const docRef = doc(infoRef, 'services')
+    const docSnap = await getDoc(docRef)
 
-    const querySnapshot = await getDocs(services);
+    const querySnapshot = await getDocs(services)
     querySnapshot.forEach((document) => {
       let service = {
         id: document.id,
         ...document.data(),
-      };
+      }
 
-      servicesData.push(service);
-    });
+      servicesData.push(service)
+    })
 
-    return { servicesData, description: docSnap.data().description };
+    return { servicesData, description: docSnap.data()?.description || '' }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 export const useGetServices = ({ enabled = true }) => {
   const { data, isLoading, error, isFetching } = useQuery(
-    ["get-all-services"],
+    ['get-all-services'],
     fetchInfo,
     {
       enabled,
       refetchOnWindowFocus: false,
     }
-  );
+  )
 
   return {
     isLoadingServices: isLoading,
     error,
     services: data?.servicesData ?? [],
-    description: data?.description ?? "",
+    description: data?.description ?? '',
     isFetchingServices: isFetching,
-  };
-};
+  }
+}
